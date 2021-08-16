96b98e400a63454c918208b19a664418f03bc508
xumoyun-fullStack <bobajanovxumoyun@gmail.com>
2021-08-16 17:29:15 +0500

    home 4

---
+++ uy_ishi4/app.js
@@ -0,0 +1,20 @@
+let result = confirm("biror son o`yladim uni topishga tayyormisiz")
+
+let trueAnswer = 5
+if(result){
+    let answer 
+    if(answer){
+        while(true){
+            answer = parseInt(prompt("taxminingiz : "))
+        
+              if(answer === trueAnswer){
+                  alert("topdingiz")
+                  break;
+              }else if(answer > trueAnswer){
+                  confirm("Men o`ylagan son bundan kichik!")
+              }else{
+                  confirm("Men o`ylagan son bundan katta!")
+              }
+          }
+    }
+}
